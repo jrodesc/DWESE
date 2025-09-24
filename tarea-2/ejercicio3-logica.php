9b4cf170f4182dbad787f84053cacc0efd561349
Jose Luis <jrodesc1511@gmail.com>
2025-09-24 09:27:43 +0200

    ejercicio 3 completado

---
+++ tarea-2/ejercicio3-logica.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $billete = $_REQUEST["billete"];
+    $edad = $_REQUEST["edad"];
+    $estudiante = $_REQUEST["estudiante"];
+    $numerosa = $_REQUEST["numerosa"];
+    $descuento = 0;
+
+    function calculoDescuento($edad, $estudiante, $numerosa, $descuento) {
+        if($edad < 4) {
+            $descuento = 100;
+        } elseif($edad > 65) {
+            $descuento = 60;
+        } elseif($estudiante == true) {
+            $descuento = 55;
+        } elseif($edad >= 4 && $edad <7) {
+            $descuento = 50;
+        } elseif($numerosa == true) {
+            $descuento = 30;
+        }
+
+        return $descuento;
+
+
+    }
+    echo "El precio inicial era: $billete";
+    echo "<br>";
+    $total = $billete - ($billete * (calculoDescuento($edad, $estudiante, $numerosa, $descuento)/100) );
+    echo "Con descuento aplicado el total es: $total";
+    ?>
+
+    <a href="ejercicio3.php">Volver al inicio</a>
+</body>
+</html>
