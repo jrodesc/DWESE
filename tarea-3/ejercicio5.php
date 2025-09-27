5e4871e3226e4b653b05bc4cb3c0c8efe88e6a40
Jose Luis <jrodesc1511@gmail.com>
2025-09-27 19:07:57 +0200

    empezando el ejercicio 5

---
+++ tarea-3/ejercicio5.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejercicio5</title>
+</head>
+<body>
+
+<!--
+Un número racional, fracciones, se caracteriza por tener un numerador y un denominador.
+Escribe un programa para operar con números racionales y simplificar los resultados. Se
+debe implementar como si fuera un menú, con un fichero por cada operación.
+Las operaciones a definir son:
+● Suma
+● Resta
+● Multiplicación
+● División
+-->
+    
+</body>
+</html>
