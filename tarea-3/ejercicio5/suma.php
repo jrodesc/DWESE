428081ce737ce69a11b784f62a69fb89d679d790
Jose Luis <jrodesc1511@gmail.com>
2025-09-28 11:38:22 +0200

    suma del ejercicio 5 realizada

--- tarea-3/ejercicio5/suma.php
+++ tarea-3/ejercicio5/suma.php
@@ -1,4 +1,5 @@
 <?php
+
 function mcd($numerador, $denominador) {
     $numerador = abs($numerador);
     $denominador = abs($denominador);
@@ -12,10 +13,27 @@ function mcd($numerador, $denominador) {
     return $numerador;
 }
 
+function mcm($numero1, $numero2) {
+    return abs($numero1 * $numero2) / mcd($numero1, $numero2);
+}
+
 function suma($numerador1, $denominador1, $numerador2, $denominador2) {
     if ($denominador1 == 0 || $denominador2 == 0) {
         throw new Exception("El denominador no puede ser cero");
     }
-    return (($numerador1*$denominador2) + ($denominador1*$numerador2) / ($denominador1 * $denominador2));
+
+    $minComMul = mcm($denominador1, $denominador2);
+
+    $num1 = $numerador1 * ($minComMul / $denominador1);
+    $num2 = $numerador2 * ($minComMul / $denominador2);
+
+    $numAjustado = $num1 + $num2;
+
+    $divisor = mcd($numAjustado, $minComMul);
+    $numSimplificado = $numAjustado / $divisor;
+    $denSimplificado = $minComMul / $divisor;
+
+
+    return [$numSimplificado, $denSimplificado];
 }
 ?>
