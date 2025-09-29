d9755ff0a228651c97695e583bd91158ef03c8be
Jose Luis <jrodesc1511@gmail.com>
2025-09-29 09:09:48 +0200

    ejercicio 1 hecho

--- tarea-4/ejercicio1/ejercicio1.php
+++ tarea-4/ejercicio1/ejercicio1.php
@@ -5,7 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<?php
+
+//vamos a meter todos los valores en un array y dar el valor de multiplicarlo
+function factorial($x) {
+    $numbers = [];
+    for($i = 1; $i != $x+1; $i++) {
+        $numbers[] = $i; 
+    }
+    return array_product($numbers);
+}
+
+?>
 <body>
-    
+    <form action="ejercicio1.php" method="post">
+        <label>Introduzca el valor m:</label>
+        <input type="number" min=0 name="m" id="m" required>
+        <br>
+        <label>Introduzca el valor n:</label>
+        <input type="number" min=0 name="n" id="n" required>
+        <br>
+        <input type="submit" name="calcular" id="calcular" value="calcular">
+    </form>
+
+    <?php
+    if(isset($_POST["calcular"])) {
+        $m = $_POST["m"];
+        $n = $_POST["n"];
+    }
+    $result = factorial($m) / (factorial($n) * factorial($m-$n));
+    echo "$result";
+    ?>
 </body>
+
 </html>
